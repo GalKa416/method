10834c31962222bbf572bcc10eb1c391a4cc534d
Галина <g416@mail.ru>
2023-04-21 22:03:26 +0500

    задача 3готова

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,8 @@ public class Main {
         printLeapYear(year);
         //task 2
         installApp();
-
+        //task 3
+        deliveryTime();
     }
 
     private static void printLeapYear(int year) {
@@ -36,6 +37,25 @@ public class Main {
             System.out.println("Задача 2.Установите версию приложения для  Android  по ссылке");
         } else if (clientOS == 1 && currentYear < 2015) {
             System.out.println("Задача 2.Установите облегченную версию приложения для  Android  по ссылке");
+        }
+    }
+
+    public static void deliveryTime()
+
+    {
+        int deliveryDistance = 95;
+        int days = -1;
+        if (deliveryDistance <= 20) {
+            days = 1;
+        } else if (deliveryDistance > 20 && deliveryDistance <= 60) {
+            days = 2;
+        } else if (deliveryDistance > 60 && deliveryDistance <= 100) {
+            days = 3;
+        }
+        if (days > 0) {
+            System.out.println("Потребуется дней: " + days);
+        } else {
+            System.out.println("Доставка не осуществляется");
         }
     }
 }
