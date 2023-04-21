befc6583d565aed89fc6737afb7289035b4c16ae
Галина <g416@mail.ru>
2023-04-21 20:33:59 +0500

    задача 1 готова

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
 public class Main {
+
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+
+        //task 1
+        System.out.print("Задача 1. ");
+        int year = 2100;
+        printLeapYear(year);
+
     }
+
+    private static void printLeapYear(int year) {
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+
+            System.out.println("Год является високосным");
+        } else {
+            System.out.println("Год не является високосным");
+        }
+
+
+    }
+
 }
